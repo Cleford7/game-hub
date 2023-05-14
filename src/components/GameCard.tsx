8b958db9de44523faea5b08833bda04399abb24c
Cleford Payusan <clefordpayusan@gmail.com>
2023-05-14 23:59:56 +0800

    Cleanup the game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,13 +13,13 @@ const GameCard = ({ game }: GameCardProps) => {
     <Card borderRadius={10} overflow="hidden" width={"300px"}>
       <Image src={getCroppedImg(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
+        <HStack justifyContent={"space-between"} marginBottom={3}>
           <PlatformIcon
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CreditScore creditScore={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
