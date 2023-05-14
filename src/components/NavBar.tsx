33ddbf2137e5f151e5e1cbb6a19beed0dfbeec14
Cleford Payusan <clefordpayusan@gmail.com>
2023-05-14 23:39:02 +0800

    Implement searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import GameSearch from "./GameSearch";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (query: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding={"10px"}>
       <Image src={logo} boxSize="60px" />
-      <GameSearch />
+      <GameSearch onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
